79af7120c8c9bdbde5b0424fb641174ad6b41123
Vold <levenvov@gmail.com>
2016-06-28 00:06:16 +0300

    edit some fuction

--- Filters/Program.cs
+++ Filters/Program.cs
@@ -13,28 +13,32 @@ namespace Filters
         static void Main(string[] args)
         {
             //Тестування Робота із класом для комплексних чисел
-            Complex num = new Complex(Convert.ToDouble(Console.ReadLine()),Convert.ToDouble(Console.ReadLine()));
-            Console.WriteLine(num);
-            Console.WriteLine(num.Magnitude);
-            Console.WriteLine(num.Phase);
+            //Complex num = new Complex(Convert.ToDouble(Console.ReadLine()),Convert.ToDouble(Console.ReadLine()));
+            //Console.WriteLine(num);
+            //Console.WriteLine(num.Magnitude);
+            //Console.WriteLine(num.Phase);
             Console.WriteLine(Math.Sin(30*Math.PI/180));
-            Filters polosovoy = new Filters(5.0, 5.0, 20.0, 0.8, 314.0);
+            Filters polosovoy = new Filters(5.0, 0.005, 20.0, 0.8, 600.0);
+            Console.WriteLine("magn: " + polosovoy.CompFilter.Magnitude + " Re: " + polosovoy.CompFilter.Real + " Im: " + polosovoy.CompFilter.Imaginary);
+            Complex x = Complex.Divide(new Complex(-2,1), new Complex(1,-1));
+            Console.WriteLine("Divide result: " + x.Real + " + j" + x.Imaginary);
             for (int i = 0; i < 100; i++)
             {
                 Console.WriteLine(polosovoy.GetUout(i));
             }
+            double timer = 0.0;
             string path = @"e:\temp\MyTest.txt";
-            if (!File.Exists(path))
+            if (File.Exists(path))
             {
-                using (StreamWriter sw = File.CreateText(path))
-                {
+                StreamWriter sw = new StreamWriter(path);
                     for (int i = 0; i < 100; i++)
                     {
-                        sw.WriteLine(polosovoy.GetUout(i));
-                        sw.WriteLine(", ");
-                    }
-                }
+                        sw.WriteLine(polosovoy.GetUout(timer));
+                        timer += 0.01;
+                    }  
+                sw.Close();
             }
+
             Console.ReadKey();
             //Кінець роботи із комплексним класом
 
@@ -77,7 +81,7 @@ namespace Filters
             Freaquence = frequence;
         }
 
-        public double GetUout(int time)//функція для виводу миттєвого значення
+        public double GetUout(double time)//функція для виводу миттєвого значення
         {
             return CompFilter.Magnitude * (Math.Sin(Freaquence * time + CompFilter.Phase));
         }
